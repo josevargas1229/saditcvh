d83bd640316a685f257b076a83b199e71b28e7ad
LUISGERARDO11 <luis34julieta@gmail.com>
2025-12-11 12:59:00 -0600

    diseño de login listo, con inputs reutlizables

--- src/app/modules/auth/pages/login/login.ts
+++ src/app/modules/auth/pages/login/login.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  standalone:false,
+  standalone: false,
   templateUrl: './login.html',
 })
-export class Login {
+export class Login implements OnInit {
+  loginForm!: FormGroup;
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      usuario: new FormControl('', { updateOn: 'blur' }), 
+      password: new FormControl('', { updateOn: 'blur' }),
+    });
+  }
+
+  onSubmit(): void {
+    this.loginForm.markAllAsTouched(); 
+    
+    if (this.loginForm.valid) {
+      console.log('Formulario Válido. Enviando datos...', this.loginForm.value);
+      // Lógica de autenticación...
+    } else {
+      console.log('Formulario Inválido. Revisar campos.');
+    }
+  }
+
+  get usuarioControl() {
+    return this.loginForm.get('usuario') as FormControl;
+  }
+  
+  get passwordControl() {
+    return this.loginForm.get('password') as FormControl;
+  }
 }
