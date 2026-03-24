1722e3198070b46faf5e89c2f64f33f82773b332
saditcvh-dev <saditcvh@gmail.com>
2026-03-24 17:28:05 -0600

    Update paginated-response.model.ts

--- src/app/core/models/paginated-response.model.ts
+++ src/app/core/models/paginated-response.model.ts
@@ -1,9 +1,13 @@
 export interface Pagination {
   total: number;
   limit: number;
-  page?: number;
+  page: number;
   totalPages?: number;
   nextCursor?: number | null;
+  // Nuevos campos del backend
+  currentPage?: number;
+  totalItems?: number;
+  itemsPerPage?: number;
 }
 
 export interface PaginatedResponse<T> {
